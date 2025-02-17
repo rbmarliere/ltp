e39e994358a50795322ca06d6d893b4abafeaa34
Ricardo B. Marlière <ricardo@marliere.net>
2025-02-17 17:22:52 -0300

    syscalls/pause03: Refactor into new API
    Signed-off-by: Ricardo B. Marlière <ricardo@marliere.net>

--- testcases/kernel/syscalls/pause/pause03.c
+++ testcases/kernel/syscalls/pause/pause03.c
@@ -1,104 +1,40 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
 /*
  * Copyright (c) International Business Machines  Corp., 2001
- *  07/2001 Ported by Wayne Boyer
- *
- * This program is free software;  you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY;  without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See
- * the GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program;  if not, write to the Free Software Foundation,
- * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ * Copyright (c) Linux Test Project, 2025
+ * 07/2001 Ported by Wayne Boyer
  */
+
 /*
- * Test Description:
- *  pause() does not return due to receipt of SIGKILL signal and specified
- *  process should be terminated.
+ * This test verifies that pause() does not return after receiving a SIGKILL
+ * signal, at which point the process should be terminated.
  */
-#include <unistd.h>
-#include <errno.h>
-#include <fcntl.h>
-#include <sys/wait.h>
 
-#include "test.h"
-#include "safe_macros.h"
+#include "tst_test.h"
 
-static pid_t cpid;
-
-char *TCID = "pause03";
-int TST_TOTAL = 1;
-
-static void do_child(void);
-static void setup(void);
-static void cleanup(void);
-
-int main(int ac, char **av)
+void run(void)
 {
-	int lc;
 	int status;
+	pid_t pid;
 
-	tst_parse_opts(ac, av, NULL, NULL);
-
-	setup();
-
-	for (lc = 0; TEST_LOOPING(lc); lc++) {
-		tst_count = 0;
-
-		if ((cpid = tst_fork()) == -1)
-			tst_brkm(TBROK | TERRNO, NULL, "fork() failed");
-
-		if (cpid == 0)
-			do_child();
-
-		TST_PROCESS_STATE_WAIT(cleanup, cpid, 'S');
-
-		kill(cpid, SIGKILL);
-
-		SAFE_WAIT(NULL, &status);
-
-		if (WIFSIGNALED(status) && WTERMSIG(status) == SIGKILL) {
-			tst_resm(TPASS, "pause() did not return after SIGKILL");
-			continue;
-		}
-
-		if (WIFSIGNALED(status)) {
-			tst_resm(TFAIL, "child killed by %s unexpectedly",
-			         tst_strsig(WTERMSIG(status)));
-			continue;
-		}
-
-		tst_resm(TFAIL, "child exited with %i", WEXITSTATUS(status));
+	pid = SAFE_FORK();
+	if (!pid) {
+		pause();
+		tst_res(TFAIL, "Unexpected return from pause()");
+		exit(0);
 	}
 
-	cleanup();
-	tst_exit();
+	TST_PROCESS_STATE_WAIT(pid, 'S', 10000);
+	kill(pid, SIGKILL);
+	SAFE_WAITPID(pid, &status, 0);
 
+	if (WIFSIGNALED(status) && WTERMSIG(status) == SIGKILL)
+		tst_res(TPASS, "pause() did not return after SIGKILL");
+	else
+		tst_res(TFAIL, "Child exited with %i", WEXITSTATUS(status));
 }
 
-void do_child(void)
-{
-	TEST(pause());
-
-	tst_resm(TFAIL, "Unexpected return from pause()");
-
-	exit(0);
-}
-
-void setup(void)
-{
-	tst_sig(FORK, DEF_HANDLER, cleanup);
-
-	TEST_PAUSE;
-}
-
-
-void cleanup(void)
-{
-	kill(cpid, SIGKILL);
-}
+static struct tst_test test = {
+	.test_all = run,
+	.forks_child = 1,
+};
